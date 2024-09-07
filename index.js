e22de8b6ed1b6cdc47d65a4e39b57e6a8870df1a
akshaypx <akshaysrivastava46@gmail.com>
2024-09-07 12:35:14 +0530

    feat : update any task with given id and new value

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("node:fs");
 
 const command = argv[2];
 
+// Adding Tasks
 if (command === "add") {
   let content = [];
   let id = 0;
@@ -11,14 +12,14 @@ if (command === "add") {
     let file = JSON.parse(fs.readFileSync("tracker.json", "utf-8"));
     id = file.length;
     let temp = {
-      id: id,
+      id: id + 1,
       task: argv[3],
     };
     const newFile = [...file, temp];
     content = JSON.stringify(newFile);
   } else {
     //create record and make json
-    id = 0;
+    id = 1;
     content = JSON.stringify([{ id: id, task: argv[3] }]);
   }
   //if not exists create else write file with above contents
@@ -26,7 +27,31 @@ if (command === "add") {
     if (err) {
       console.error(err);
     } else {
-      console.log(`task added successfully ID:${id}`);
+      console.log(`Task added successfully (ID:${id})`);
     }
   });
+}
+
+// Edit
+
+if (command === "update") {
+  let id = argv[3];
+  let task = argv[4];
+  if (fs.existsSync("tracker.json")) {
+    let file = JSON.parse(fs.readFileSync("tracker.json", "utf-8"));
+    if (parseInt(id) > file.length || parseInt(id) <= 0)
+      throw new Error("Task does not exist, please create first");
+    else {
+      let newFile = file.map((f, _) => {
+        if (f.id === parseInt(id)) {
+          return { ...f, task: task };
+        } else return f;
+      });
+      newFile = JSON.stringify(newFile);
+      fs.writeFile("tracker.json", newFile, (err) => {
+        if (err) console.error(err);
+        else console.log(`Task (ID:${id}) updated successfully`);
+      });
+    }
+  } else throw new Error("Task does not exist, please create first");
 }
